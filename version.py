7019359aca4365751b834f32c4716abe00811e66
anroy <anroyflash@gmail.com>
2015-11-01 02:32:56 +0900

    Update version.py

--- version.py
+++ version.py
@@ -1,3 +1,10 @@
 #! python
 import sys
-sys.stdout.write("Python version %s\n" % (sys.version,))
+import numpy
+import nltk
+import pip
+
+sys.stdout.write("Python version: %s\n" % (sys.version,))
+sys.stdout.write("numpy version: %s\n" % (numpy.__version__,))
+sys.stdout.write("nltk version: %s\n" % (nltk.__version__,))
+sys.stdout.write("pip version: %s\n" % (pip.__version__,))
