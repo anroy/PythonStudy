a470bcd5b1108a03e25d5cf04656e27e40ec4c5b
anroy <anroyflash@gmail.com>
2016-10-08 19:37:29 +0900

    Ignored ipynb files

--- ar_tokenize.py
+++ ar_tokenize.py
@@ -2,7 +2,9 @@
 import nltk
 import sys
 
-sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+# sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+sentence = "Now is the time for all good men to come to the aid of the party."
+
 tokens = nltk.word_tokenize(sentence)
 sys.stdout.write("tokens: %s\n" % (tokens,))
 
